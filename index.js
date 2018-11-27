a9033c7082e0cc2759250ce79b93435f69368e56
Richard Schaubeck <richard.schaubeck@gmail.com>
2018-11-27 19:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,13 +11,21 @@ let customerId = 0;
 
 class Customer {
   constructor(name, neighborhood) {
-    this.id = ++customerId
+    this.id = ++customerId;
+    this.name = name;
+    if(neighborhood) {
+      this.neighborhoodId = neighborhood.id;
+    }
   }
 }
 
-class Meal {
-  constructor() {
+let mealId = 0;
 
+class Meal {
+  constructor(title, price) {
+    this.id = ++mealId
+    this.title = title;
+    this.price = price;
   }
 }
 
